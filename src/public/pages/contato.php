d088b27e20485595c0a5f317341d41762bff4847
Eduardo Belem <edumanoel@gmail.com>
2025-01-22 19:52:16 -0300

    add page loading functionality and contact form

---
+++ src/public/pages/contato.php
@@ -0,0 +1,23 @@
+<h2>Contato</h2>
+
+<div class="container">
+    <form action="pages/forms/contato.php" method="post">
+        <div class="form-group">
+            <label for="name">Nome</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Digite seu nome">
+        </div>
+        <div class="form-group">
+            <label for="email">E-mail</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu e-mail">
+        </div>
+        <div class="form-group">
+            <label for="subject">Assunto</label>
+            <input type="text" class="form-control" id="subject" name="subject" placeholder="Digite o assunto">
+        </div>
+        <div class="form-group">
+            <label for="message">Mensagem</label>
+            <textarea class="form-control" id="message" name="message" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+</div>
